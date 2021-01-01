622d03b676d56f4d156855d0f7357684356d1976
bookgvi <bookgvi@gmail.com>
2021-01-01 21:26:54 +0300

    Класс использующий семафор

---
+++ src/com/example/hello/Semaphore/LoginQueue.java
@@ -0,0 +1,23 @@
+package com.example.hello.Semaphore;
+
+import java.util.concurrent.Semaphore;
+
+public class LoginQueue {
+  private Semaphore semaphore;
+
+  public LoginQueue(int slots) {
+    semaphore = new Semaphore(slots);
+  }
+
+  public boolean trLogin() {
+    return semaphore.tryAcquire();
+  }
+
+  public void logout() {
+    semaphore.release();
+  }
+
+  public int emptySlots() {
+    return semaphore.availablePermits();
+  }
+}
