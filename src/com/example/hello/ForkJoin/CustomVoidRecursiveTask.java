e239ea38c12e7bea38fc85abdbdbe4eada43ce76
bookgvi <bookgvi@gmail.com>
2021-01-05 21:20:18 +0300

    Class extends RecursiveAction - рекурсивное создание потоков-подзадач

---
+++ src/com/example/hello/ForkJoin/CustomVoidRecursiveTask.java
@@ -0,0 +1,40 @@
+package com.example.hello.ForkJoin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveAction;
+import java.util.logging.Logger;
+
+public class CustomVoidRecursiveTask extends RecursiveAction {
+  private String workload;
+  private static final int THRESHOLD = 4;
+  private Logger logger = Logger.getAnonymousLogger();
+
+  public CustomVoidRecursiveTask(String workload) {
+    this.workload = workload;
+  }
+
+  @Override
+  protected void compute() {
+    if (workload.length() > CustomVoidRecursiveTask.THRESHOLD) {
+      ForkJoinTask.invokeAll(splitToSubTasks());
+    } else {
+      proceed();
+    }
+  }
+
+  private void proceed() {
+    String upperCaseString = workload.toUpperCase();
+    logger.info(workload + " -> " + upperCaseString);
+  }
+
+  private List<CustomVoidRecursiveTask> splitToSubTasks() {
+    List<CustomVoidRecursiveTask> subTasks = new ArrayList<>();
+    String newWorkloadPartOne = workload.substring(0, workload.length() / 2);
+    String newWorkloadPartTwo = workload.substring(workload.length() / 2);
+    subTasks.add(new CustomVoidRecursiveTask(newWorkloadPartOne));
+    subTasks.add(new CustomVoidRecursiveTask(newWorkloadPartTwo));
+    return subTasks;
+  }
+}
