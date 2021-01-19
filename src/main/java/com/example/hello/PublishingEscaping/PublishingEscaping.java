f987d6a7955fdcb60306a7b32492efad38a3bce4
bookgvi <bookgvi@gmail.com>
2021-01-20 00:47:49 +0300

    класс для демнстрации Ускользания

---
+++ src/main/java/com/example/hello/PublishingEscaping/PublishingEscaping.java
@@ -0,0 +1,9 @@
+package com.example.hello.PublishingEscaping;
+
+public class PublishingEscaping {
+  private String[] strArray = new String[] {"Q", "W", "E"};
+
+  public String[] getStrArray() {
+    return strArray;
+  }
+}
